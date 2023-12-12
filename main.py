0b6fa1a93aaa0ab682f6d3df60849ce734be89e9
Dmitrii <ddeal087@yandex.ru>
2023-12-12 11:08:14 +0300

    deleted:    Google.py 	deleted:    main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as r
-from speech_recognition.__main__ import source
-
-r.adjust_for_ambient_noise(source)
-with r.Microphone(device_index=2) as source:
-
-    print("Speak Anything :")
-    audio = r.listen(source)
